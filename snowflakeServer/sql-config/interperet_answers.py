08dfddce55deed9b170db38faf4b8cca776ac1fe
Eden Barlow <10960976@uvu.edu>
2024-03-02 04:40:21 -0700

    updated to avg

--- snowflakeServer/sql-config/interperet_answers.py
+++ snowflakeServer/sql-config/interperet_answers.py
@@ -1,5 +1,5 @@
 def interperet_answers(answers, correlation_functions, correlation_coefficients):
-    weighted_sum = 0
+    sum = 0
     for i in range(len(answers)):
-        weighted_sum += correlation_functions[i](answers[i]) * correlation_coefficients[i]
-    return weighted_sum
+        sum += correlation_functions[i](answers[i])
+    return sum / len(answers)
