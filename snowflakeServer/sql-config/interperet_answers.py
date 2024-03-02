ddb7de9f72e82d19d3cd8eece9f71069542f3568
Eden Barlow <10960976@uvu.edu>
2024-03-02 09:12:44 -0700

    fixed interperet

--- snowflakeServer/sql-config/interperet_answers.py
+++ snowflakeServer/sql-config/interperet_answers.py
@@ -1,5 +1,10 @@
 def interperet_answers(answers, correlation_functions, correlation_coefficients):
     sum = 0
     for i in range(len(answers)):
-        sum += correlation_functions[i](answers[i])
+        value = correlation_functions[i](answers[i])
+        if value > 100:
+            value = 100
+        elif value < 0:
+            value = 0
+        sum += value
     return sum / len(answers)
