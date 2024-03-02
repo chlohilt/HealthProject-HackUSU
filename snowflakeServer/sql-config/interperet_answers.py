d6ab21e745a9d33c331f997788e0eb82533a492b
Eden Barlow <10960976@uvu.edu>
2024-03-02 04:17:18 -0700

    interperet_answers set up

--- snowflakeServer/sql-config/interperet_answers.py
+++ snowflakeServer/sql-config/interperet_answers.py
@@ -1,3 +1,5 @@
 def interperet_answers(answers, correlation_functions, correlation_coefficients):
+    weighted_sum = 0
     for i in range(len(answers)):
-        pass
+        weighted_sum += correlation_functions[i](answers[i]) * correlation_coefficients[i]
+    return weighted_sum
